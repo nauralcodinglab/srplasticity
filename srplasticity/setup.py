72e193146ed56c1b59a823d636c4096fd2018d60
jgbeninger <jbeninger@yahoo.ca>
2021-01-28 15:11:21 -0500

    Added setup file and named top directory to match package name

---
+++ srplasticity/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='srplasticity',
+      version='0.1',
+      description='Functions for linear-nonlinear synaptic convolution model',
+      url='https://github.com/nauralcodinglab/srplasticity.git',
+      author='Julian Rossbroich, Daniel Trotter, John Beninger, Richard Naud',
+      license='GPLv3',
+      packages=['srplasticity'],
+      zip_safe=False)
