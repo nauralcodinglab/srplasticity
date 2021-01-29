60727df6d6f3b706febf25509288745c8114536f
jgbeninger <jbeninger@yahoo.ca>
2021-01-28 19:08:18 -0500

    Added license and license headers

--- srplasticity/tm.py
+++ srplasticity/tm.py
@@ -6,6 +6,21 @@ This module contains an implementation of the Tsodyks-Markram model of short-ter
 - classic TM model
 - adapted TM model to capture supralinear facilitation
 - method to fit the TM model to data using an exhaustive parameter grid search
+ 
+Copyright (C) 2021 Julian Rossbroich, Daniel Trotter, John Beninger, Richard Naud
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import numpy as np
--- srplasticity/tools.py
+++ srplasticity/tools.py
@@ -3,6 +3,21 @@ _tools.py Module
 
 This module contains tools and helper functions that are used by other modules
 or that are useful for implementations
+
+Copyright (C) 2021 Julian Rossbroich, Daniel Trotter, John Beninger, Richard Naud
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import numpy as np
